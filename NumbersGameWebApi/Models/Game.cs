9237a205b4b7e39944ab3b7fb34f0b5b3c3945be
Duncan Mackay <dmackay@scottlogic.co.uk>
2015-06-10 17:45:46 +0100

    Added NumbersGameWebApi
    Uses ASP.NET Web API

---
+++ NumbersGameWebApi/Models/Game.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using ScottLogic.NumbersGame;
+
+namespace AspWebAppSample.Models
+{
+    public class Game
+    {
+        
+        public Game(int bigNumbers)
+        {
+            int target;
+            int[] numbers;
+            GameGenerator.GenerateCountdownGame(bigNumbers, out numbers, out target);
+            StartingValues = numbers;
+            Target = target;
+        }
+
+        public int[] StartingValues { get; set; }
+        
+
+        public int Target { get; set; }
+        public int BigNumbers { get; set; }
+    }
+}
