c0e601d8e918d4dd0f99ce5fb438f98c59f326a7
daslo <dmackay@scottlogic.co.uk>
2015-06-17 11:21:39 +0100

    Web API client now playing games!

--- MvvmWebApiClient/ViewModels/NumbersGameViewModel.cs
+++ MvvmWebApiClient/ViewModels/NumbersGameViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Diagnostics;
 using System.Linq;
 using System.Text;
@@ -16,7 +17,9 @@ namespace MvvmWebApiClient.ViewModels
     {
         public NumbersGameViewModel()
         {
-            CurrentGame = new NumbersGame();
+            //CurrentGame = new NumbersGame();
+            CurrentGame = new NumbersGame(new[] { 100, 25, 10, 7, 5, 2 }, 715);
+
             AdditionCommand = new DelegateCommand<object>( OnAddition, 
                                                            CanDoAddition);
 
@@ -32,7 +35,7 @@ namespace MvvmWebApiClient.ViewModels
             ////
             OperationHistory = new List<IOperation>();
             SelectedNumbers = new List<int>();
-
+            Debug.WriteLine("NumbersGameViewModel created, hash= {0}", GetHashCode());
 
         }
         /// <summary>
@@ -68,33 +71,63 @@ namespace MvvmWebApiClient.ViewModels
             get { return CurrentGame.CurrentNumbers; }
         }
 
-        public IList<int> SelectedNumbers { get; set; }
+        public IList<int> SelectedNumbers
+        {
+            get { return _selectedNumbers; }
+            set
+            {
+                _selectedNumbers.Clear();
+                foreach (var v in value)
+                        _selectedNumbers.Add(v);
+                
+                // So far, this ISN'T enough for things to just work.
+                
+                
+                // And this doesn't do it:
+                OnPropertyChanged("SelectedNumbers");
+
+                // This is wrong, BUT it does let the UI work for now:
+                (AdditionCommand as DelegateCommand<object>).RaiseCanExecuteChanged();
+                (SubtractionCommand as DelegateCommand<object>).RaiseCanExecuteChanged();
+                (MultiplicationCommand as DelegateCommand).RaiseCanExecuteChanged();
+                (DivisionCommand as DelegateCommand).RaiseCanExecuteChanged();
+                
+            }
+
+        }
         public IList<IOperation> OperationHistory { get; set; }
 
         private INumbersGame _game;
-
+        private readonly ObservableCollection<int> _selectedNumbers = new ObservableCollection<int>(); 
 
         // Command implementations (all private)
         private void OnAddition(object ob)
         {
-            IOperation op = new Operation(Op1, Op2, Operator.Addition);
-
+            ApplyOperation(new Operation(Op1, Op2, Operator.Addition));
         }
+
+        // Helper for all operations
+        private void ApplyOperation(IOperation op)
+        {
+            _game.DoOperation(op);
+            SelectedNumbers = new int[] { };
+            OnPropertyChanged("CurrentGame");
+        }
+
+
         private void OnSubtraction(object ob)
         {
-            IOperation op = new Operation(Op1, Op2, Operator.Subtraction);
-
+            ApplyOperation(new Operation(Op1, Op2, Operator.Subtraction));
         }
+
         private void OnMultiplication()
         {
-            IOperation op = new Operation(Op1, Op2, Operator.Subtraction);
-
+            ApplyOperation(new Operation(Op1, Op2, Operator.Multiplication));
         }
+
         private void OnDivision()
         {
-            IOperation op = new Operation(Op1, Op2, Operator.Subtraction);
-
-
+            ApplyOperation(new Operation(Op1, Op2, Operator.Division));
         }
 
         
@@ -111,6 +144,7 @@ namespace MvvmWebApiClient.ViewModels
         
         private bool CanDoSubtraction(object ob)
         {
+            Debug.WriteLine("CanDoSubtraction...");
             // We don't care about the order of selection; we do care if the numbers are the same though, we don't want to generate zeros
             return (SelectedNumbers.Count() == 2 &&
                     SelectedNumbers[0] != SelectedNumbers[1]);
@@ -118,12 +152,14 @@ namespace MvvmWebApiClient.ViewModels
 
         private bool CanDoMultiplication()
         {
+            Debug.WriteLine("CanDoMultiplication...");
             return SelectedNumbers.Count() == 2;
             // We *could* also forbid multiplication by 1 as it doesn't do anything "useful". (It replaces a '1' and an 'x' with just an 'x')
             // However, if we were looking for solutions where ALL the numbers must get used up, then it would indeed be useful...
         }
         private bool CanDoDivision()
         {
+            Debug.WriteLine("CanDoDivision...");
             if (SelectedNumbers.Count() != 2) return false;
 
             int lhs = Math.Max(SelectedNumbers[0], SelectedNumbers[1]);
--- MvvmWebApiClient/Views/NumbersGameView.xaml.cs
+++ MvvmWebApiClient/Views/NumbersGameView.xaml.cs
@@ -1,19 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
-using Microsoft.Practices.Prism.Mvvm;
+﻿using System.Collections.Generic;
 using System.Diagnostics;
+using System.Linq;
+using System.Windows.Controls;
+using System.Windows.Input;
+using Microsoft.Practices.Prism.Mvvm;
 using MvvmWebApiClient.ViewModels;
 
 namespace MvvmWebApiClient.Views
@@ -58,6 +48,9 @@ namespace MvvmWebApiClient.Views
                 // YES! Thank you resharper:
                 List<int> selectedInts = selItems.Cast<int>().ToList();
                 viewModel.SelectedNumbers = selectedInts;
+
+                // Force CanExecute requeries
+               // CommandManager.InvalidateRequerySuggested();
             }
         }
     }
