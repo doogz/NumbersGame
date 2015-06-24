36d1afa8022cf3d378c003f618de2b1e21e998e6
daslo <dmackay@scottlogic.co.uk>
2015-06-24 17:50:01 +0100

    Added MVVMLight web api client project

---
+++ LightWebClient/Design/DesignDataService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using LightWebClient.Model;
+
+namespace LightWebClient.Design
+{
+    public class DesignDataService : IDataService
+    {
+        public void GetData(Action<DataItem, Exception> callback)
+        {
+            // Use this to create design time data
+
+            var item = new DataItem("DesignDataService");
+            callback(item, null);
+        }
+    }
+}
---
+++ LightWebClient/ViewModel/MainViewModel.cs
@@ -0,0 +1,89 @@
+﻿using GalaSoft.MvvmLight;
+using LightWebClient.Model;
+
+namespace LightWebClient.ViewModel
+{
+    /// <summary>
+    /// This class contains properties that the main View can data bind to.
+    /// <para>
+    /// See http://www.galasoft.ch/mvvm
+    /// </para>
+    /// </summary>
+    public class MainViewModel : ViewModelBase
+    {
+        private readonly IDataService _dataService;
+
+        /// <summary>
+        /// The <see cref="MainHeading" /> property's name.
+        /// </summary>
+        public const string MainHeadingPropertyName = "MainHeading";
+
+        private string _mainHeading = string.Empty;
+        /// <summary>
+        /// Gets the WelcomeTitle property.
+        /// Changes to that property's value raise the PropertyChanged event. 
+        /// </summary>
+        public string MainHeading
+        {
+            get { return _mainHeading; }
+
+            set
+            {
+                if (_mainHeading == value)
+                {
+                    return;
+                }
+
+                _mainHeading = value;
+                RaisePropertyChanged(MainHeadingPropertyName);
+            }
+        }
+
+        private string _strapLine = string.Empty;
+
+        public string StrapLine
+        {
+            get { return _strapLine; }
+
+            set
+            {
+                if (_strapLine == value)
+                {
+                    return;
+                }
+
+                _strapLine = value;
+                RaisePropertyChanged(MainHeadingPropertyName);
+            }
+        }
+
+
+        /// <summary>
+        /// Initializes a new instance of the MainViewModel class.
+        /// </summary>
+        public MainViewModel(IDataService dataService)
+        {
+            MainHeading = "LightWebClient";
+            StrapLine = "- An MVVM Light implementation of the NumbersGame Web Client -";
+            _dataService = dataService;
+            _dataService.GetData(
+                (item, error) =>
+                {
+                    if (error != null)
+                    {
+                        // Report error here
+                        return;
+                    }
+
+                    // Do nothing for nowWelcomeTitle = item.Title;
+                });
+        }
+
+        ////public override void Cleanup()
+        ////{
+        ////    // Clean up if needed
+
+        ////    base.Cleanup();
+        ////}
+    }
+}
