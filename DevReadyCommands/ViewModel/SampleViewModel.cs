086cab2c534d80b65b61bd3a89b990204e529214
Daslo <dmackay@scottlogic.com>
2015-06-25 00:08:29 +0100

    Additional changes (from home) for DevReady/demo/training code project

---
+++ DevReadyCommands/ViewModel/SampleViewModel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevReadyCommands.ViewModel
+{
+    public class SampleViewModel : ViewModelBase
+    {
+        public SampleViewModel()
+        {
+            InitializeViewModel();
+        }
+        
+        protected override void InitializeViewModel()
+        {
+
+
+        }
+        public override string ViewName
+        {
+            get { return "Thinking in MVVM"; }
+        }
+
+        public string LabelCaption { get { return "This is my content"; } }
+        public string Color1Caption { get { return "Colour One"} }
+        public string Color2Caption { get { return "Colour Two"} }
+        public DelegateCommand<>
+    }
+}
---
+++ DevReadyCommands/ViewModel/ViewModelBase.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+
+namespace DevReadyCommands.ViewModel
+{
+    public class ViewModelBase
+    {
+        event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void InitializeViewModel()
+        {}
+
+        
+        public abstract string ViewName { get; }
+
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+
+    }
+}
